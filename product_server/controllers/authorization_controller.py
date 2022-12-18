18a14ae9c8d99f457001b5a7db0827ec735f527d
Mursina <fathimamursina2015@gmail.com>
2022-12-18 15:03:20 +0530

    Change the authentication method to basicAuth

--- product_server/controllers/authorization_controller.py
+++ product_server/controllers/authorization_controller.py
@@ -6,10 +6,4 @@ https://connexion.readthedocs.io/en/latest/security.html
 def check_api_key(api_key, required_scopes):
     return {'test_key': 'test_value'}
 
-def check_productstore_auth(token):
-    return {'scopes': ['read:products', 'write:products'], 'uid': 'test_value'}
-
-def validate_scope_productstore_auth(required_scopes, token_scopes):
-    return set(required_scopes).issubset(set(token_scopes))
-
 
